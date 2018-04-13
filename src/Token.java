52a5911df5ea0ebe674a35295e0093ced774ed64
3I_10K <eliocamison@gmail.com>
2018-04-13 18:03:27 +0200

    Add class main

--- src/Token.java
+++ src/Token.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Token {
 
     enum Toktype {
@@ -7,6 +10,7 @@ public class Token {
     private Toktype ttype;
     private int value;
     private char tk;
+
 
     // Constructor privat. Evita que es puguin construir objectes Token externament
     // S'han de crear el objectes desde la mateixa classe
@@ -24,28 +28,47 @@ public class Token {
 
     // Torna un token de tipus "OP"
     static Token tokOp(char c) {
-        Token tkOperador = new Token();
-        return null;
+        Token t = new Token();
+        t.ttype = Toktype.OP;
+        t.tk = c;
+        return t;
     }
 
     // Torna un token de tipus "PAREN"
     static Token tokParen(char c) {
-        Token tkParen = new Token();
-        return null;
+        Token t = new Token();
+        t.ttype = Toktype.PAREN;
+        t.tk = c;
+        return t;
     }
 
     // Mostra un token (conversió a String)
     public String toString() {
+
         return "";
     }
 
     // Mètode equals. Comprova si dos objectes Token són iguals
     public boolean equals(Object o) {
+
         return false;
     }
 
     // A partir d'un String, torna una llista de tokens
+    // Aquí rebem com a paràmetre un string amb anotació inversa; 1+311
     public static Token[] getTokens(String expr) {
+        char[] operador = new char[]{'+','-','*','/'};
+
+        List<String> list = new ArrayList();
+
+        for (int i = 0; i < expr.length(); i++) {
+            if (expr.charAt(i) == 'A') {
+
+            }
+        }
+
+        list.add(expr);
+
         return null;
     }
 
