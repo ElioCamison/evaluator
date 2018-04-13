6728d5e85c74ba86f446d1ff946e6d7f3720e959
3I_10K <eliocamison@gmail.com>
2018-04-13 18:04:34 +0200

    Add class main

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Evaluator {
 
 
     public static int calculate(String expr) {
+
+        List<String> calc = new ArrayList();
         // Convertim l'string d'entrada en una llista de tokens
         Token[] tokens = Token.getTokens(expr);
         // Efectua el procediment per convertir la llista de tokens en notació RPN
@@ -11,6 +15,7 @@ public class Evaluator {
     }
 
     public static int calcRPN(Token[] list) {
+
         // Calcula el valor resultant d'avaluar la llista de tokens
         return 0;
         
