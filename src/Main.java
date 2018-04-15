7863b6867906e017da8bd5f59ada8a141f76ca5b
Elio <eliocamison@gmail.com>
2018-04-15 11:57:55 +0200

    Lost season 2 T_T

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        String proves = "1+311";
+        Map<Character,Integer> mapa = new HashMap();
 
+        for (int i = 0; i < proves.length(); i++) {
+            mapa.put(proves.charAt(i),i);
+        }
     }
+
 }
