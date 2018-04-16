8687007ee21d4b4357358661a45cb9cfc0c5adf2
3I_10K <eliocamison@gmail.com>
2018-04-16 19:22:24 +0200

    Class Evaluator

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Evaluator {
@@ -9,13 +10,18 @@ public class Evaluator {
         List<String> calc = new ArrayList();
         // Convertim l'string d'entrada en una llista de tokens
         Token[] tokens = Token.getTokens(expr);
+
         // Efectua el procediment per convertir la llista de tokens en notació RPN
         // Finalment, crida a calcRPN amb la nova llista de tokens i torna el resultat
         return 0;
     }
 
     public static int calcRPN(Token[] list) {
+        LinkedList<Token> linkedToken = new LinkedList();
 
+        for (int i = 0; i < list.length; i++) {
+            linkedToken.push(list[i]);
+        }
         // Calcula el valor resultant d'avaluar la llista de tokens
         return 0;
         
